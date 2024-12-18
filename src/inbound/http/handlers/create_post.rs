8012e845e49cc8043f5fe2f746f54174e751fb44
fan-tastic-z <fantastic.fun.zf@gmail.com>
2024-12-18 15:02:54 +0800

    feat: add create post test

--- src/inbound/http/handlers/create_post.rs
+++ src/inbound/http/handlers/create_post.rs
@@ -15,12 +15,12 @@ use crate::{
 };
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
-pub struct CreateAuthorHttpRequestBody {
+pub struct CreatePostHttpRequestBody {
     pub title: String,
     pub content: String,
 }
 
-impl CreateAuthorHttpRequestBody {
+impl CreatePostHttpRequestBody {
     fn try_into_domain(self) -> Result<CreatePostRequest, CreatePostError> {
         let req = CreatePostRequest::new(self.title, self.content)?;
         Ok(req)
@@ -48,7 +48,7 @@ impl From<&Post> for CreatePostResponseData {
 
 pub async fn create_post<BS: BlogService>(
     State(state): State<AppState<BS>>,
-    Json(body): Json<CreateAuthorHttpRequestBody>,
+    Json(body): Json<CreatePostHttpRequestBody>,
 ) -> Result<ApiSuccess<CreatePostResponseData>, ApiError> {
     let domain_req = body.try_into_domain()?;
     state
@@ -57,4 +57,74 @@ pub async fn create_post<BS: BlogService>(
         .await
         .map_err(ApiError::from)
         .map(|ref post| ApiSuccess::new(StatusCode::CREATED, post.into()))
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::anyhow;
+    use std::{
+        mem,
+        sync::{Arc, Mutex},
+    };
+    use uuid::Uuid;
+
+    use super::*;
+
+    #[derive(Clone)]
+    struct MockBlogService {
+        create_post_result: Arc<Mutex<Result<Post, CreatePostError>>>,
+    }
+
+    impl BlogService for MockBlogService {
+        async fn create_post(&self, _req: &CreatePostRequest) -> Result<Post, CreatePostError> {
+            let mut guard = self.create_post_result.lock();
+            let mut result = Err(CreatePostError::Unknown(anyhow!("substitute error")));
+            mem::swap(guard.as_deref_mut().unwrap(), &mut result);
+            result
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_create_post_success() {
+        let title = "title".to_string();
+        let content = "content".to_string();
+        let post_id = Uuid::new_v4().to_string();
+        let created_at = Utc::now();
+        let service = MockBlogService {
+            create_post_result: Arc::new(Mutex::new(Ok(Post {
+                id: post_id.clone(),
+                title: title.clone(),
+                content: content.clone(),
+                created_at,
+            }))),
+        };
+        let state = State(AppState {
+            blog_service: Arc::new(service),
+        });
+        let body = Json(CreatePostHttpRequestBody {
+            title: title.clone(),
+            content: content.clone(),
+        });
+        let expected = ApiSuccess::new(
+            StatusCode::CREATED,
+            CreatePostResponseData {
+                id: post_id.clone(),
+                title,
+                content,
+                created_at,
+            },
+        );
+        let actual = create_post(state, body).await;
+        assert!(
+            actual.is_ok(),
+            "expected create_post to succeed, but got {:?}",
+            actual
+        );
+        let actual = actual.unwrap();
+        assert_eq!(
+            actual, expected,
+            "expected ApiSuccess {:?}, but got {:?}",
+            expected, actual
+        );
+    }
 }
